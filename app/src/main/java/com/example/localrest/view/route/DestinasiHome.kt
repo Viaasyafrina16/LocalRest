2d9f0a0951ccb8a8da543fccec7b0449a6c4dd10
Viaasyafrina16 <syafrinavia@gmail.com>
2025-12-18 16:02:36 +0700

    membuat route DestinasiHome

---
+++ app/src/main/java/com/example/localrest/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.localrest.view.route
+
+import com.example.localrest.R
+
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route= "home"
+    override val titleRes = R.string.app_name
+}
