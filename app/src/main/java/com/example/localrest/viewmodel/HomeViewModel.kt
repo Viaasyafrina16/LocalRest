d44a544861689734e18bdd26d43d5a0982978837
Viaasyafrina16 <syafrinavia@gmail.com>
2025-12-18 15:59:27 +0700

    Menambahkan HomeViewModel untuk mengelola state dan pemuatan data siswa

---
+++ app/src/main/java/com/example/localrest/viewmodel/HomeViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.localrest.viewmodel
+
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.localrest.Repositpory.RepositoryDataSiswa
+import com.example.localrest.modeldata.DataSiswa
+import kotlinx.coroutines.launch
+import java.io.IOException
+import retrofit2.HttpException
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<DataSiswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
+
+class HomeViewModel(private val repositoryDataSiswa: RepositoryDataSiswa) :
+    ViewModel() {
+    var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init{
+        loadSiswa()
+    }
+
+    fun loadSiswa(){
+        viewModelScope.launch {
+            listSiswa = StatusUiSiswa.Loading
+            listSiswa = try{
+                StatusUiSiswa.Success(repositoryDataSiswa.getDataSiswa())
+            } catch (e: IOException){
+                StatusUiSiswa.Error
+            } catch (e: HttpException){
+                StatusUiSiswa.Error
+            }
+        }
+    }
+}
