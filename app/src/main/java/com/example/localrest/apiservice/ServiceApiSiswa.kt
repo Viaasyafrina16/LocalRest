f27868c0312b5c390627ece052ae8b76bc6713ff
Viaasyafrina16 <syafrinavia@gmail.com>
2025-12-25 12:32:49 +0700

    menambah operasi CRUD untuk siswa tertentu

--- app/src/main/java/com/example/localrest/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/localrest/apiservice/ServiceApiSiswa.kt
@@ -2,19 +2,26 @@ package com.example.localrest.apiservice
 
 import com.example.localrest.modeldata.DataSiswa
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 interface ServiceApiSiswa {
-    @GET("bacaTeman.php")
+    @GET("bacateman.php")
     suspend fun getSiswa(): List<DataSiswa>
 
     @POST("insertTM.php")
     suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+
+
+   @GET("baca1Teman.php/{id}")
+    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
+
+    @PUT("editTM.php/{id}")
+    suspend fun editSatuSiswa(@Query("id") id: Int,@Body dataSiswa :DataSiswa):retrofit2.Response<Void>
+
+    @DELETE("deleteTM.php/{id}")
+    suspend fun hapusSatuSiswa(@Query("id")id: Int):retrofit2.Response<Void>
 }
-
-//    @GET("baca1Teman.php/{id}")
-//    suspend fun getStasusSiswa(@Query("id") id: Int): DataSiswa
-
-//    @PUT("editTM.php/{id}")
-//    suspend fun editStatusSiswa(@Qyery("id") id: Int): DataSiswa
