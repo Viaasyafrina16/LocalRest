8440712ce3ea710098e9a919dad981cd03ba1866
Viaasyafrina16 <syafrinavia@gmail.com>
2025-12-25 14:24:57 +0700

    menagganti import

--- app/src/main/java/com/example/localrest/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/localrest/viewmodel/EditViewModel.kt
@@ -11,8 +11,10 @@ import com.example.localrest.modeldata.DetailSiswa
 import com.example.localrest.modeldata.UIStateSiswa
 import com.example.localrest.modeldata.toDataSiswa
 import com.example.localrest.modeldata.toUiStateSiswa
+import com.example.localrest.view.route.DestinasiDetail
 import kotlinx.coroutines.launch
-import okhttp3.Response
+import retrofit2.Response
+
 
 class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
 RepositoryDataSiswa
@@ -38,7 +40,7 @@ RepositoryDataSiswa
     }
     suspend fun editSatuSiswa(){
         if (validasiInput(uiStateSiswa.detailSiswa)){
-            val call:Response<Void> = repositoryDataSiswa.editSatuSiswa(idSiswa,uiStateSiswa
+            val call: Response<Void> = repositoryDataSiswa.editSatuSiswa(idSiswa,uiStateSiswa
                 .detailSiswa.toDataSiswa())
 
             if (call.isSuccessful){
